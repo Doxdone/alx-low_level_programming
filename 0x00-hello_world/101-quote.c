49034f710f58fd2b90004e8c07884e634430c5a5
doxdone <doxdone@gmail.com>
2022-10-17 00:11:00 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+*main - Entry point
+*Return: Alwys 1
+*/
+
+int main(void)
+{
+	write(2, "and that piece of art is usful\" - Dora Korpar, 2015-10-\n", 59);
+	return (1);
+}
